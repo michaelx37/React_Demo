b1510c5a48dcd38ffa6de20674cd1535d592a358
michalex37 <michaelx2837@gmail.com>
2018-07-10 09:33:24 +0200

    prova mic

--- React_demo/Startup.cs
+++ React_demo/Startup.cs
@@ -7,16 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-
-
-
-
-
-
-
 using Microsoft.AspNetCore.Http;
 using React.AspNet;
-
+//gnooooo
 
 namespace React_demo
 {
