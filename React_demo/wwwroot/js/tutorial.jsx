9ec4b54d01c1c6e2cbb2b9425beff4387b642ceb
michalex37 <michaelx2837@gmail.com>
2018-07-11 12:34:47 +0200

    roba

--- React_demo/wwwroot/js/tutorial.jsx
+++ React_demo/wwwroot/js/tutorial.jsx
@@ -3,7 +3,7 @@
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList />
+                <CommentList data={this.props.data} />
                 <CommentForm />
             </div>
         );
@@ -12,25 +12,59 @@
 
 class CommentList extends React.Component {
     render() {
+        const commentNodes = this.props.data.map(comment => (
+            <Comment author={comment.Author} key={comment.Id}>
+                {comment.Text}
+            </Comment>
+        ));
         return (
             <div className="commentList">
-                Hello, world! I am a CommentList.
+                {commentNodes}
             </div>
         );
     }
 }
+
+
 
 class CommentForm extends React.Component {
     render() {
         return (
             <div className="commentForm">
-                Hello, world! I am a CommentForm.
+                <p>Hello, world! I am a CommentForm.</p>
             </div>
         );
     }
 }
 
+class Comment extends React.Component {
+    rawMarkup() {
+        const md = new Remarkable();
+        const rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    }
+    render() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+        );
+    }
+}
+/*
+const data = [
+    { id: 1, author: "Daniel Lo Nigro", text: "Hello ReactJS.NET World!" },
+    { id: 2, author: "Pete Hunt", text: "This is one comment" },
+    { id: 3, author: "Jordan Walke", text: "This is *another* comment" }
+];
+*/
+
 
 ReactDOM.render(
-    <CommentBox />, document.getElementById('content')
+    <CommentBox url="/comments" />, document.getElementById('content')
 );
+// Note that in a real app, you should generate the URL server-side (via Url.Action call) and
+// pass it down, or use RouteJs rather than hard - coding it.This tutorial hard - codes it for simplicity.
