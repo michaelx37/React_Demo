4a6320b34e25026986d73bfea77c1955e8c43c45
michalex37 <michaelx2837@gmail.com>
2018-07-13 12:20:10 +0200

    added comment form

--- React_demo/Controllers/HomeController.cs
+++ React_demo/Controllers/HomeController.cs
@@ -45,12 +45,25 @@ namespace React_demo.Controllers
             return View();
         }
 
+
+
         [Route("comments")]
         [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
         public ActionResult Comments()
         {
             return Json(_comments);
         }
+
+        [Route("comments/new")]
+        [HttpPost]
+        public ActionResult AddComment(CommentModel comment)
+        {
+            // Create a fake ID for this comment
+            comment.Id = _comments.Count + 1;
+            _comments.Add(comment);
+            return Content("Success :)");
+        }
+
 
     }
 }
--- React_demo/wwwroot/js/tutorial.jsx
+++ React_demo/wwwroot/js/tutorial.jsx
@@ -1,14 +1,44 @@
 ﻿class CommentBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { data: [] };
+        this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+    }
+    loadCommentsFromServer() {
+        const xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = () => {
+            const data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        };
+        xhr.send();
+    }
+    handleCommentSubmit(comment) {
+        const data = new FormData();
+        data.append('author', comment.author);
+        data.append('text', comment.text);
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('post', this.props.submitUrl, true);
+        xhr.onload = () => this.loadCommentsFromServer();
+        xhr.send(data);
+    }
+    componentDidMount() {
+        this.loadCommentsFromServer();
+        window.setInterval(() => this.loadCommentsFromServer(), this.props.pollInterval);
+    }
     render() {
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList data={this.props.data} />
-                <CommentForm />
+                <CommentList data={this.state.data} />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
             </div>
         );
     }
 }
+
+
 
 class CommentList extends React.Component {
     render() {
@@ -28,14 +58,53 @@ class CommentList extends React.Component {
 
 
 class CommentForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { author: '', text: '' };
+        this.handleAuthorChange = this.handleAuthorChange.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleAuthorChange(e) {
+        this.setState({ author: e.target.value });
+    }
+    handleTextChange(e) {
+        this.setState({ text: e.target.value });
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        const author = this.state.author.trim();
+        const text = this.state.text.trim();
+        if (!text || !author) {
+            return;
+        }
+        this.props.onCommentSubmit({ Author: author, Text: text });
+        this.setState({ author: '', text: '' });
+    }
     render() {
         return (
-            <div className="commentForm">
-                <p>Hello, world! I am a CommentForm.</p>
-            </div>
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Your name"
+                    value={this.state.author}
+                    onChange={this.handleAuthorChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Say something..."
+                    value={this.state.text}
+                    onChange={this.handleTextChange}
+                />
+                <input type="submit" value="Post" />
+            </form>
         );
     }
 }
+
+
+
+
 
 class Comment extends React.Component {
     rawMarkup() {
@@ -64,7 +133,7 @@ const data = [
 
 
 ReactDOM.render(
-    <CommentBox url="/comments" />, document.getElementById('content')
+    <CommentBox url="/comments" submitUrl="/comments/new" pollInterval={2000} />, document.getElementById('content')
 );
 // Note that in a real app, you should generate the URL server-side (via Url.Action call) and
 // pass it down, or use RouteJs rather than hard - coding it.This tutorial hard - codes it for simplicity.
