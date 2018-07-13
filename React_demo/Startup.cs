1d6c51a48c0baf423ccb57bf5f8c05839517c3b1
michaelx37 <michaelx2837@gmail.com>
2018-07-13 16:54:47 +0200

    rimuovere

--- React_demo/Startup.cs
+++ React_demo/Startup.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using React.AspNet;
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,9 +11,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-using Microsoft.AspNetCore.Http;
-using React.AspNet;
-//gnooooo
 
 namespace React_demo
 {
